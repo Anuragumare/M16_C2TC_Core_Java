4da0780f7b1bf781e96b86f09bc477f5cda5a827
ANURAG <ANURAG@LUCIFER>
2022-04-14 14:20:51 +0530

    first commit

---
+++ DAY3_OOPS/src/in/cg/oops/Example.java
@@ -0,0 +1,13 @@
+package in.cg.oops;
+
+public class Example {
+
+		Example (){
+			System.out.println(" wlecome to capgimini ");
+		}
+		public static void main(String[] args) {
+			Example ex = new Example();
+
+	}
+
+}
---
+++ DAY3_OOPS/src/in/cg/oops/car.java
@@ -0,0 +1,14 @@
+package in.cg.oops;
+
+public class car {
+	String name = " bmw" ;
+	 void print() {
+		 System.out.println("this is name"+name);
+	 }
+
+	public static void main(String[] args) {
+		car obj = new car();
+		obj.print();
+	}
+
+}
