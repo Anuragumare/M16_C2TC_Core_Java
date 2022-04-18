55580f838f417d444fd2aa159d1aa37359523f13
ANURAG <ANURAG@LUCIFER>
2022-04-18 13:50:02 +0530

    FIRST COMMIT

---
+++ DAY7_this_static_instanceof/src/in/cg/this1/Example1.java
@@ -0,0 +1,28 @@
+package in.cg.this1;
+
+	class Parent
+	{
+		int a;
+		void print(int a)
+		{
+			this.a=a;
+		}
+		void disply() {
+			System.out.println("the value of a "+a);
+		}
+	}
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		Parent p = new Parent ();
+		p.print(12); 
+		p.disply();
+		
+		
+
+	}
+
+}
+
+
---
+++ DAY7_this_static_instanceof/src/in/cg/this1/Example2.java
@@ -0,0 +1,19 @@
+package in.cg.this1;
+
+public class Example2 
+{
+	String str;
+	Example2(String str)
+	{
+		this.str=str;
+	}
+
+	public static void main(String[] args) {
+		Example2 e = new Example2("java full Stack");
+		System.out.println(e.str);
+		
+
+
+
+	}
+}
